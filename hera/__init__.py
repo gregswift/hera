6f833fffdb9422d359a8603128f94d46e2d9793a
Greg Swift <gregswift@gmail.com>
2013-02-01 16:00:19 -0600

    Added Constants to library Added a wsdl name cleaning function for consistency extended init attributes started storing attributes in private (_) namespace moved basic work to methods added SOAP 1.2 support TODO: doesn't seem to be working yet Attempt to be able to change between WSDLs without creating a new instance Toying with idea of changing the mapper functiosn to just use getattr override.  This looses dynamic wsdl loading capability though. Added inline test when api is run directly.

--- hera/__init__.py
+++ hera/__init__.py
@@ -5,39 +5,124 @@ from suds.client import Client
 from suds.transport.http import HttpAuthenticated
 from suds.xsd.doctor import ImportDoctor, Import
 
+VERSION = '1.2'
+WSDL_PATH = '/usr/share/zeus/wsdl'
+DEFAULT_WSDL = 'System.Cache'
+HEADERS = { 'Content-Type': 'text/xml' }
+HEADERS_1_2 = { 'Content-Type': 'application/soap+xml' }
+
+def cleanWSDLName(wsdl):
+    """Strips leading path and .wsdl ext from path name for consistency"""
+    return os.path.basename(wsdl.replace('.wsdl',''))
+
 class Hera:
 
-    def __init__(self, username, password, location, wsdl="System.Cache.wsdl"):
-
+    def __init__(self, username, password, location, version=VERSION,
+            wsdl="System.Cache", wsdl_path=WSDL_PATH):
+        
+        self.location = location
+        self.version = version
         # Sorry windows
-        cur = os.path.dirname(__file__)
-        url = "file://%s" % os.path.abspath(os.path.join(cur, 'wsdl', wsdl))
+        self._wsdl_path = os.path.abspath(wsdl_path)
+        self._wsdl_orig = wsdl
+        self._wsdl = self._getWSDL(wsdl, self._wsdl_path, version)
 
         # Apparently Zeus's wsdl is broken and we have to jimmy this thing in
         # manually.  See https://fedorahosted.org/suds/ticket/220 for details.
-        # Also, you'll be happy to know that the zillion .wsdl files that Zeus
-        # includes apparently have different targetNamespace's.  This one
-        # happens to be 1.1, but if you load something else you'll probably
-        # need to adjust it.
         imp = Import('http://schemas.xmlsoap.org/soap/encoding/')
-        imp.filter.add('http://soap.zeus.com/zxtm/1.1/')
-        doctor = ImportDoctor(imp)
 
-        transporter = HttpAuthenticated(username=username, password=password)
+        ### I'm getting rid of this... don't think its needed anymore?
+        # Also, you'll be happy to know that the zillion .wsdl files that Zeus
+        # includes apparently have different targetNamespace's. So..
+        # we are defaulting to 1.2 now, but 
+        #imp.filter.add('http://soap.zeus.com/zxtm/1.2/')
 
-        self.client = Client(url, doctor=doctor, location=location,
-                             transport=transporter)
+        self._doctor = ImportDoctor(imp)
+
+        self._transport = HttpAuthenticated(username=username, password=password)
+
+        self._loadWSDL(self._wsdl)
+
+    def _getWSDL(self, wsdl, path, version):
+        wsdl = cleanWSDLName(wsdl)
+        versioned_wsdl = '{0}_{1}'.format(wsdl, version.replace('.','_'))
+        path = os.path.abspath(path)
+        available = self.availableWSDLs(path)
+        for w in (versioned_wsdl, wsdl):
+            if w in available:
+                return 'file://{0}.wsdl'.format(os.path.join(path,w))
+        raise Exception, 'Unable to locate WSDL {0}'.format(wsdl)
+
+    def availableWSDLs(self, path):
+        available = []
+        for wsdl in os.listdir(path):
+            if os.path.isfile(os.path.join(path,wsdl)):
+                available.append(cleanWSDLName(wsdl))
+        return available
+
+    def _loadWSDL(self, wsdl, version=None, doctor=None, transport=None, location=None):
+        if version is None:
+            version = self.version
+        _headers = HEADERS
+        if version == '1.2':
+            from suds.bindings import binding
+            binding.envns = ('SOAP-ENV', 'http://www.w3.org/2003/05/soap-envelope')
+            _headers = HEADERS_1_2
+        if doctor is None:
+            doctor = self._doctor
+        if location is None:
+            location = self.location
+        if transport is None:
+            (username, password) = self._transport.credentials()
+            transport = HttpAuthenticated(username=username, password=password)
+        self.client = Client(wsdl, doctor=doctor, transport=transport,
+                location=location, headers=_headers)
+        self._headers = _headers
+        self._doctor = doctor
+        self._location = location
+        self._transport = transport
+
+    def loadWSDL(self, wsdl, wsdl_path=None, version=None):
+        if wsdl == self._wsdl:
+            return
+        if wsdl_path is None:
+            wsdl_path = self._wsdl_path
+        if version is None:
+            version = self.version
+        new_wsdl = self._getWSDL(wsdl, wsdl_path, version)
+        self._loadWSDL(new_wsdl)
+        self._wsdl_path = wsdl_path
+        self._wsdl = new_wsdl
+
+    def __getattr__(self, name):
+        def method(*args):
+            return getattr(self.client.service, name)(*args)
+        return method
+
+    def getVirtualServerNames(self):
+        """Returns list of Virtual Servers"""
+        wsdl = 'VirtualServer'
+        self.loadWSDL(wsdl)
+        return self.client.service.getVirtualServerNames()
+
+    def getEnabledVirtualServers(self):
+        """Returns list of enabled Virtual servers"""
+        wsdl = 'VirtualServer'
+        self.loadWSDL(wsdl)
+        return self.client.service.getEnabled()
+
+#    def getGlobalCacheInfo(self):
+#        """Returns a small object of statistics."""
+#        wsdl = 'System.Cache'
+#        self.loadWSDL(wsdl)
+#        return self.client.service.getGlobalCacheInfo()
 
     def flushAll(self):
         """Flushes everything in the system: all objects across all virtual
         servers."""
+        wsdl = 'System.Cache'
+        self.loadWSDL(wsdl)
         return self.client.service.clearWebCache()
-
-
-    def getGlobalCacheInfo(self):
-        """Returns a small object of statistics."""
-        return self.client.service.getGlobalCacheInfo()
-
 
     def flushObjectsByPattern(self, url, return_list=False):
         """Flush objects out of the cache.  This accepts simple wildcards (*)
@@ -45,6 +130,8 @@ class Hera:
         URLs that matched the pattern.  There is a performance hit when
         returning the list since we have to request it, build it, and return
         it.  """
+        wsdl = 'System.Cache'
+        self.loadWSDL(wsdl)
         if return_list:
             objects = self.getObjectsByPattern(url)
 
@@ -65,9 +152,31 @@ class Hera:
         return self.getObjectsByPattern(url, 1)
 
     def getObjectsByPattern(self, url, limit=None):
+        wsdl = 'System.Cache'
+        self.loadWSDL(wsdl)
         o = urlparse(url)
         r = self.client.service.getCacheContent(o.scheme, o.netloc,
                                                 o.path, limit)
 
         if r.number_matching_items:
             return r.matching_items
+
+if __name__ == '__main__':
+    import sys
+    from getpass import getpass, getuser
+
+    location = raw_input('What is the URL for the load balancer? ')
+    wsdl_path = raw_input('Where are your WSDL files stored? [{0}] '.format(WSDL_PATH))
+    if not wsdl_path:
+        wsdl_path = WSDL_PATH
+    wsdl = raw_input('Which WSDL do you want to load? [{0}] '.format(DEFAULT_WSDL))
+    if not wsdl:
+        wsdl = DEFAULT_WSDL
+    user = getuser()
+    username = raw_input('What is your username? [{0}] '.format(user))
+    if not username:
+        username = user
+    print 'Now we need to authenticate {0} on {1}'.format(username,location)
+    password = getpass('What is your password? ')
+    h = Hera(username, password, location, wsdl=wsdl, wsdl_path=wsdl_path)
+    h.getGlobalCacheInfo()
